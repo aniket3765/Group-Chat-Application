4a88b77775d4953c61f874c953ae599564977dca
aniket3765 <121852693+aniket3765@users.noreply.github.com>
2023-10-01 07:49:26 +0530

    first commit

---
+++ public/js/signupPage.js
@@ -0,0 +1,4 @@
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const phoneNumber = document.getElementById('phoneNumber');
+const password = document.getElementById('password');
